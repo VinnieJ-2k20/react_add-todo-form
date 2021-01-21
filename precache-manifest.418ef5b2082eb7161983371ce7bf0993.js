3774bd7c6d87c2276c97f1c031c4f9f226ebdc53
PaulT <ptimofejev@gmail.com>
2021-01-21 15:10:02 +0200

    Updates

---
+++ precache-manifest.418ef5b2082eb7161983371ce7bf0993.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f38c1d4aa9e7091b32cdb0aed4cb3ad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0f53fdf5895670831509",
+    "url": "./static/css/main.0f3a84e1.chunk.css"
+  },
+  {
+    "revision": "2c058ce433359b5022d1",
+    "url": "./static/js/2.6040eaac.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.6040eaac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f53fdf5895670831509",
+    "url": "./static/js/main.3badb3da.chunk.js"
+  },
+  {
+    "revision": "de889ebc4391832068f6",
+    "url": "./static/js/runtime-main.311b6371.js"
+  }
+]);
